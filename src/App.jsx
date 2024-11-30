a4e0eec3196ff19c1ce8c112c20c3ed304231071
Dipak Kumawat <dipaksk11@gmail.com>
2024-11-30 22:14:22 +0530

    about

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import About from "./component/About";
 import Hero from "./component/Hero";
 
 const App = () => {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
       <Hero />
-      <section className="z-0 min-h-screen bg-blue-500" ></section>
+      <About/>
     </main>
   );
 };
